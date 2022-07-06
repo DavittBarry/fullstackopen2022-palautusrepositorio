073fe3b399b816c8a0d7e19eaeff5c5a1a74aa42
Davitt Barry <63282613+DavittBarry@users.noreply.github.com>
2022-07-06 10:41:56 +0300

    Add files via upload

---
+++ osa1/1.3: kurssitiedot step3/src/App.js
@@ -0,0 +1,106 @@
+// luodan ensin komponentit Header, Content ja Total. Ne ottavat dataa App-komponentin olioista.
+
+// Part komponentti luo jokainen kurssi erikseen props:ien avulla.
+
+const Part = ({ partid, exercise }) => {
+  return (
+    <p>'{partid}' is made up of {exercise} exercises.</p>
+  );
+};
+
+const Header = ({ course }) => {
+  return (
+    <div>
+      <h1>{course}</h1>
+    </div>
+  );
+};
+
+
+const Content = ({ part1, exercise1, part2, exercise2, part3, exercise3 }) => {
+  return(
+  <div>
+    <Part 
+      partid={part1}
+      exercise={exercise1}
+    />
+    <Part 
+      partid={part2}
+      exercise={exercise2}
+    />
+    <Part 
+      partid={part3}
+      exercise={exercise3}
+    />
+
+    
+
+    {/*<p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+  </p>*/}
+  </div>
+  )
+}
+
+const Total = ({ exercise1, exercise2, exercise3 }) => {
+  return (
+  <div>
+    <p>Total number of exercises {exercise1 + exercise2 + exercise3}</p>
+    </div>
+  )
+}
+
+const App = () => {
+
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+    }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+    }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+    }
+
+
+/*
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  */
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content 
+        part1={part1.name} 
+        part2={part2.name}
+        part3={part3.name}
+        exercise1={part1.exercises}
+        exercise2={part2.exercises}
+        exercise3={part3.exercises} 
+      />
+      <Total
+        exercise1={part1.exercises}
+        exercise2={part2.exercises}
+        exercise3={part3.exercises} 
+      />
+    </div>
+  );
+};
+
+export default App;
